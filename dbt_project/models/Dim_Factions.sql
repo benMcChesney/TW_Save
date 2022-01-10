9b3582e06cfe91ec4b471b16c2bb9a9b4fd9ed39
DESKTOP-85J35KO\benmc <ben.mcchesney@gmail.com>
2022-01-10 07:49:28 -0800

    added data_LabelMapping for better faction names.

--- dbt_project/models/Dim_Factions.sql
+++ dbt_project/models/Dim_Factions.sql
@@ -17,12 +17,22 @@ with dFactions_cte AS
         , CHARINDEX( '_' , fn , CHARINDEX( '_' , fn , CHARINDEX( '_' , fn , CHARINDEX( '_' , fn )+1 )+1)+1) as us_3
     FROM dFactions_cte
 )
+, labeled_cte AS 
+(
+	select 
+		ROW_NUMBER() OVER( ORDER BY fn ) as id 
+		, SUBSTRING( fn, 0 , us_0 ) as game_source
+		, SUBSTRING( fn, us_0+1 , us_1 - us_0 - 1) as dlc_source
+		, SUBSTRING( fn, us_1+1 , us_2 - us_1 - 1) as faction
+		, REPLACE( SUBSTRING( fn, us_2+1 , LEN(fn) - us_2 ) , '_' , ' ' ) as lord
+		, faction_nk 
+
+	FROM faction_split_cte
+)
 
 select 
-	ROW_NUMBER() OVER( ORDER BY fn ) as id 
-	, SUBSTRING( fn, 0 , us_0 ) as game_source
-	, SUBSTRING( fn, us_0+1 , us_1 - us_0 - 1) as dlc_source
-	, SUBSTRING( fn, us_1+1 , us_2 - us_1 - 1) as faction
-	, SUBSTRING( fn, us_2+1 , LEN(fn) - us_2 ) as lord
-	, faction_nk 
-FROM faction_split_cte
+	labeled_cte.*
+	, dfl.factionLabel
+FROM labeled_cte
+LEFT OUTER JOIN Data_FactionLabels as dfl 
+ON labeled_cte.faction = dfl.faction
